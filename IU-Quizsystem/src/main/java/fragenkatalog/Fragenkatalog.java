72657044a62dfc28e7762980d36530b80c9acfbc
Jule Engel <jule.engel1@iu-study.org>
2024-02-07 17:07:58 +0100

    Fragenkatalog Tutor Ansicht / Bearbeitung

--- IU-Quizsystem/src/main/java/fragenkatalog/Fragenkatalog.java
+++ IU-Quizsystem/src/main/java/fragenkatalog/Fragenkatalog.java
@@ -13,6 +13,7 @@ public class Fragenkatalog {
 	private String incorrect_answer_2;
 	private String incorrect_answer_3;
 	private int difficulty;
+	private String module_short;
 	private String module;
 	private String explanation;
 	private String status;
@@ -22,7 +23,7 @@ public class Fragenkatalog {
 	
 	public Fragenkatalog(int id, String question, String correct_answer, String incorrect_answer_1,
             String incorrect_answer_2, String incorrect_answer_3, int difficulty,
-            String module, String explanation, String status) {
+            String module_short, String module, String explanation, String status) {
 	   this.id = id;
 	   this.question = question;
 	   this.correct_answer = correct_answer;
@@ -30,6 +31,7 @@ public class Fragenkatalog {
 	   this.incorrect_answer_2 = incorrect_answer_2;
 	   this.incorrect_answer_3 = incorrect_answer_3;
 	   this.difficulty = difficulty;
+	   this.module_short = module_short;
 	   this.module = module;
 	   this.explanation = explanation;
 	   this.status = status;
@@ -89,6 +91,15 @@ public class Fragenkatalog {
 
 	public void setDifficulty(int difficulty) {
 		this.difficulty = difficulty;
+	}
+
+	
+	public String getModule_short() {
+		return module_short;
+	}
+
+	public void setModule_short(String module_short) {
+		this.module_short = module_short;
 	}
 
 	public String getModule() {
--- IU-Quizsystem/src/main/java/fragenkatalog/FragenkatalogController.java
+++ IU-Quizsystem/src/main/java/fragenkatalog/FragenkatalogController.java
@@ -13,6 +13,8 @@ import java.util.List;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import login.User;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -23,79 +25,146 @@ import javax.persistence.Persistence;
 public class FragenkatalogController implements Serializable
 {
     private Fragenkatalog neueFrage = new Fragenkatalog();
-    private int index = 0;
+    private Fragenkatalog tempFrage;
     private FragenkatalogDAO fragenkatalogDAO = new FragenkatalogDAO();
-    private String moduleFilter;
+    private String moduleFilterNew;
+    private String moduleFilterReported;
+    private String moduleFilterInProgress;
+    private String moduleFilterPublished;
     
     @Inject
     private FragenkatalogListe fragenkatalogListe = new FragenkatalogListe();
     
-    private int maxIndex = fragenkatalogListe.getSize() - 1;;
-    private boolean isEmpty = fragenkatalogListe.getIsEmpty();
     
-    
-    
-    public Fragenkatalog getFragenkatalogListeAnIndex() {
-        return fragenkatalogListe.getFragenkatalogListe().get(index);
-    }
-    
-    public List<Fragenkatalog> getFilteredResults() {
+    public List<Fragenkatalog> questionsResults(String filter) {
+    	String moduleFilter;
+    	if (filter == "new") {
+    		moduleFilter = moduleFilterNew;
+    	}
+    	else if (filter == "reported") {
+    		moduleFilter = moduleFilterReported;
+    	}
+    	else if (filter == "in_progress") {
+    		moduleFilter = moduleFilterInProgress;
+    	}
+    	else if (filter == "published") {
+    		moduleFilter = moduleFilterPublished;
+    	}
+    	else {
+    		moduleFilter = null;
+    	}
+    	
     	if (moduleFilter == null || moduleFilter.trim().isEmpty()) {
-            return fragenkatalogListe.getFragenkatalogListe(); // Kein Filter, zeige die gesamte Liste
-        }
+    		List<Fragenkatalog> filteredList = new ArrayList<>();
 
-        List<Fragenkatalog> filteredList = new ArrayList<>();
-        String filterText = moduleFilter.toLowerCase();
+            for (Fragenkatalog fragenkatalog : fragenkatalogListe.getFragenkatalogListe()) {
+                if (fragenkatalog.getStatus().equals(filter)) {
+                    filteredList.add(fragenkatalog);
+                }
+            }
+            return filteredList;
+        }
+    	
+    	String filterText = moduleFilter.toLowerCase();
+    	List<Fragenkatalog> filteredList = new ArrayList<>();
 
         for (Fragenkatalog fragenkatalog : fragenkatalogListe.getFragenkatalogListe()) {
-            if (fragenkatalog.getModule().toLowerCase().contains(filterText)) {
-                filteredList.add(fragenkatalog);
+        	if (fragenkatalog.getModule().toLowerCase().contains(filterText) 
+            		|| fragenkatalog.getModule_short().toLowerCase().contains(filterText)) {
+            	if (fragenkatalog.getStatus().equals(filter)) {
+                    filteredList.add(fragenkatalog);
+                }
             }
         }
-
         return filteredList;
     }
     
-    public void loesungsListeMix() {
-    	//TODO Mischung der verschiedenen Lösungen und Anzeige für Liste
+    public String linkToEdit(Fragenkatalog frage) {
+    	this.tempFrage = frage;
+    	return "fragenkatalogFrageAnpassenTutor?faces-redirect=true";
     }
     
-    public void vor() {
-        if (index < maxIndex) {
-            index++;
-        }
+    public String formatDifficulty(int difficulty) {
+    	if (difficulty == 1) return "Leicht";
+    	else if (difficulty == 2) return "Mittel";
+    	else if (difficulty == 3) return "Schwer";
+    	else return "Keine Angabe";
     }
     
-    public void zurueck() {
-    	if (index > 0) {
-    		index--;
-    	}
+    public void checkTempFrage() {
+    	if (this.tempFrage == null) {
+	        FacesContext facesContext = FacesContext.getCurrentInstance();
+	        facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null, "fragenkatalogAnzeigeTutor?faces-redirect=true");
+	    }
     }
     
-    public int getIndex() {
-    	return index;
+	 public String deleteFrage(Fragenkatalog frage)  {
+        fragenkatalogDAO.deleteFrage(frage);
+        fragenkatalogListe.getFragenkatalogListe().remove(frage);
+        return "fragenkatalogAnzeigeTutor?faces-redirect=true";
     }
-    
-    public int getMaxIndex() {
-    	return maxIndex;
+	 
+	 public String updateFrage(Fragenkatalog frage) {
+		if (frage.getStatus().equals("new") || frage.getStatus().equals("reported")) {
+			frage.setStatus("in_progress");
+		}
+        fragenkatalogDAO.updateFrage(frage);
+        return "fragenkatalogAnzeigeTutor?faces-redirect=true";
     }
-    
-    public boolean getIsEmpty() {
-        return isEmpty;
-    }
+	 
+	 public String publishFrage(Fragenkatalog frage) {
+		 	frage.setStatus("published");
+	        fragenkatalogDAO.updateFrage(frage);
+	        return "fragenkatalogAnzeigeTutor?faces-redirect=true";
+	    }
+	 
+	 public String cancelEdit() {
+		 return "fragenkatalogAnzeigeTutor?faces-redirect=true";
+	 }
     
     public Fragenkatalog getNeueFrage() {
 
     	return this.neueFrage;
     }
+    
+    public Fragenkatalog getTempFrage() {
 
-	public String getModuleFilter() {
-		return moduleFilter;
+    	return this.tempFrage;
+    }
+
+	public String getModuleFilterNew() {
+		return moduleFilterNew;
 	}
 
-	public void setModuleFilter(String moduleFilter) {
-		this.moduleFilter = moduleFilter;
+	public void setModuleFilterNew(String moduleFilterNew) {
+		this.moduleFilterNew = moduleFilterNew;
 	}
+
+	public String getModuleFilterReported() {
+		return moduleFilterReported;
+	}
+
+	public void setModuleFilterReported(String moduleFilterReported) {
+		this.moduleFilterReported = moduleFilterReported;
+	}
+
+	public String getModuleFilterInProgress() {
+		return moduleFilterInProgress;
+	}
+
+	public void setModuleFilterInProgress(String moduleFilterInProgress) {
+		this.moduleFilterInProgress = moduleFilterInProgress;
+	}
+
+	public String getModuleFilterPublished() {
+		return moduleFilterPublished;
+	}
+
+	public void setModuleFilterPublished(String moduleFilterPublished) {
+		this.moduleFilterPublished = moduleFilterPublished;
+	}
+	
+	
     
     
     /*
@@ -121,21 +190,6 @@ public class FragenkatalogController implements Serializable
         emissionDAO.updateEmission(emission);
     }
     
-    public void deleteEmission(Emission emission)  {
-        
-        emissionDAO.deleteEmission(emission);
-        emissionenListe.getEmissionenListe().remove(emission);
-        
-        if (maxIndex != 0) {
-            maxIndex -= 1;
-        }
-        if (index > maxIndex) {
-            index = maxIndex;
-        }
-        if (emissionenListe.getEmissionenListe().isEmpty()) {
-            isEmpty = true;
-        }
-    }
     
     public void validateEmission(FacesContext context, UIComponent comp, Object value) throws ValidatorException{
         String regex = "^[0-9,]+$";

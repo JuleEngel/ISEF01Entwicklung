794151a73ec5ec258fbf969f4d700e3668c1426b
Philipp Pulst <philipp.pulst@iu-study.org>
2024-02-07 12:40:03 +0100

    Erstellung package quiz

---
+++ IU-Quizsystem/src/main/java/quiz/Question.java
@@ -0,0 +1,30 @@
+package quiz;
+
+import java.util.List;
+
+public class Question {
+	private String questionText;
+	private List<String> answers;
+    private String correctAnswer;
+	
+    public String getQuestionText() {
+		return questionText;
+	}
+	public void setQuestionText(String questionText) {
+		this.questionText = questionText;
+	}
+	public List<String> getAnswers() {
+		return answers;
+	}
+	public void setAnswers(List<String> answers) {
+		this.answers = answers;
+	}
+	public String getCorrectAnswer() {
+		return correctAnswer;
+	}
+	public void setCorrectAnswer(String correctAnswer) {
+		this.correctAnswer = correctAnswer;
+	}
+	
+    
+}

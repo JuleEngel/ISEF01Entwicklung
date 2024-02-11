cb7d230883ceebd94eff6bced7fac48ae59d4ccd
Philipp Pulst <philipp.pulst@iu-study.org>
2024-02-11 01:20:35 +0100

    Solo.xhtml angelegt, Duo.xhtml angelegt, QuestionDAO angefangen

--- IU-Quizsystem/src/main/java/quiz/Question.java
+++ IU-Quizsystem/src/main/java/quiz/Question.java
@@ -1,23 +1,59 @@
 package quiz;
 
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Question {
-	private String questionText;
-	private List<String> answers;
+
+	@Id
+	private int id;
+	private String question;
+	private String incorrectAnswer1;
+	private String incorrectAnswer2;
+	private String incorrectAnswer3;
     private String correctAnswer;
-	
-    public String getQuestionText() {
-		return questionText;
+    
+    
+    public Question(int id, String question, String incorrectAnswer1, String incorrectAnswer2, String incorrectAnswer3,
+			String correctAnswer) {
+		super();
+		this.id = id;
+		this.question = question;
+		this.incorrectAnswer1 = incorrectAnswer1;
+		this.incorrectAnswer2 = incorrectAnswer2;
+		this.incorrectAnswer3 = incorrectAnswer3;
+		this.correctAnswer = correctAnswer;
 	}
-	public void setQuestionText(String questionText) {
-		this.questionText = questionText;
+	public int getId() {
+		return id;
 	}
-	public List<String> getAnswers() {
-		return answers;
+	public void setId(int id) {
+		this.id = id;
 	}
-	public void setAnswers(List<String> answers) {
-		this.answers = answers;
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getIncorrectAnswer1() {
+		return incorrectAnswer1;
+	}
+	public void setIncorrectAnswer1(String incorrectAnswer1) {
+		this.incorrectAnswer1 = incorrectAnswer1;
+	}
+	public String getIncorrectAnswer2() {
+		return incorrectAnswer2;
+	}
+	public void setIncorrectAnswer2(String incorrectAnswer2) {
+		this.incorrectAnswer2 = incorrectAnswer2;
+	}
+	public String getIncorrectAnswer3() {
+		return incorrectAnswer3;
+	}
+	public void setIncorrectAnswer3(String incorrectAnswer3) {
+		this.incorrectAnswer3 = incorrectAnswer3;
 	}
 	public String getCorrectAnswer() {
 		return correctAnswer;
@@ -26,5 +62,7 @@ public class Question {
 		this.correctAnswer = correctAnswer;
 	}
 	
+	
+  
     
 }
--- IU-Quizsystem/src/main/java/quiz/QuestionDAO.java
+++ IU-Quizsystem/src/main/java/quiz/QuestionDAO.java
@@ -1,5 +1,18 @@
 package quiz;
 
-public class QuestionDAO {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class QuestionDAO {
+	private final static EntityManagerFactory emf = Persistence.createEntityManagerFactory("quizsystem");
+	
+	public List<Question> loadQuestion() {
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createQuery("select a from Question a");
+        return q.getResultList();
+    }
 }

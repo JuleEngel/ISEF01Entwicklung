b30a8b5ee0a796eeb89fd43818b72d1463390fc2
Jule Engel <jule.engel1@iu-study.org>
2024-02-21 20:48:36 +0100

    QuizSystem Solo Frage 1

--- IU-Quizsystem/src/main/webapp/quiz/soloQuiz.js
+++ IU-Quizsystem/src/main/webapp/quiz/soloQuiz.js
@@ -1,3 +1,20 @@
-function onSubmit() {
+function startQuiz() {
+	localStorage.setItem('solvedState', 'false');
+}
+
+function onSubmit(correctAnswer) {
 	document.getElementById("solutionText").style.display = "block";
+	
+	var allButtons = document.querySelectorAll('.ui-selectonebutton .ui-button');
+    allButtons.forEach(function(button) {
+		button.style.backgroundColor = 'blue';
+	});
+    
+    var selectedButton = $('.ui-selectonebutton .ui-button.ui-state-active');
+    selectedButton.css('background-color', 'yellow');
+    
+    var correctAnswer = correctAnswer;
+    console.log(selectedButton);
+    console.log(correctAnswer);
+    console.log(allButtons);
 }
